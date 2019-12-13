62860bcd734876ac23535dd57288313db8f6850d
Nnaji Victor <nnajivictor0@gmail.com>
2019-12-13 11:35:29 +0100

    add connection string and datacontext

--- API/Startup.cs
+++ API/Startup.cs
@@ -27,10 +27,11 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // services.AddDbContext<DataContext>(opt => 
-            // {
-            //     opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
-            // });
+            services.AddDbContext<DataContext>(opt => 
+            {
+                opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+            });
+            
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 

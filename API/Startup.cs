11402b366e25b5356e23e58ebbe12c294555297b
Nnaji Victor <nnajivictor0@gmail.com>
2019-12-14 14:39:13 +0100

    add activities list

--- API/Startup.cs
+++ API/Startup.cs
@@ -31,6 +31,12 @@ namespace API
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+
+            services.AddCors( opt => {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
@@ -48,6 +54,7 @@ namespace API
                 // app.UseHsts();
             }
 
+            app.UseCors("CorsPolicy");
             // app.UseHttpsRedirection();
             app.UseMvc();
         }
--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import axios from "axios";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    values : []
+  }
+
+  componentDidMount(){
+    axios.get("http://localhost:5000/api/Values")
+    .then((response) => {
+      console.log(response)
+      this.setState({
+        values : response.data
+      })
+    })
+  }
+
+
+  render(){
+    return (
+      <div className="App">
+        {this.state.values.map((value : any) =>  (
+          <li>{value.name}</li>
+        ))}
+      </div>
+    );
+  }
+  
 }
 
 export default App;
